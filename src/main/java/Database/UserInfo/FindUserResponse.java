20f88b498bf37d01f08cfd41114e4e34f8345266
yatimUrFU <yatim2002@gmail.com>
2021-11-11 14:23:42 +0500

    rename

--- src/main/java/Database/UserInfo/FindUserResponse.java
+++ src/main/java/Database/UserInfo/FindUserResponse.java
@@ -4,19 +4,19 @@ import Database.MovieRepository.MovieRepo;
 
 public class FindUserResponse {
 
-    public String dialogue(Long chatId, UserRepo chatInfo, MovieRepo movieRepo, String text){
-        Chat chat = chatInfo.getChat(chatId);
+    public String dialogue(Long chatId, UserRepo userRepo, MovieRepo movieRepo, String text){
+        Chat chat = userRepo.getChat(chatId);
         switch (chat.searchCriteria.getChatState()) {
             case START:
                 chat.searchCriteria.setChatState(ChatState.CHOICE_GENRE);
                 chat.searchCriteria.setGenre(text);
-                chatInfo.update(chat);
+                userRepo.update(chat);
                 return "Выберите страну";
             case CHOICE_GENRE:
                 chat.searchCriteria.setChatState(ChatState.CHOICE_COUNTRY);
                 chat.searchCriteria.setCountry(text);
                 chat.searchCriteria.setChatState(ChatState.RESULT); //end of search
-                chatInfo.update(chat);
+                userRepo.update(chat);
             case RESULT:
                 return movieRepo.findMovie(chat.searchCriteria).getTitle();
             default:
