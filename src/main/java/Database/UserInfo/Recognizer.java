4f7588f30ae437c46192de09e83ec09bceda7a51
yatimUrFU <yatim2002@gmail.com>
2021-12-22 02:02:31 +0500

    rename variables

--- src/main/java/Database/UserInfo/Recognizer.java
+++ src/main/java/Database/UserInfo/Recognizer.java
@@ -14,9 +14,9 @@ public class Recognizer {
     public int indexArray;
     public String output;
     private int distance = Integer.MAX_VALUE;
-    ArrayList<String> a1 = new ArrayList<>();
-    ArrayList<String> b1 = new ArrayList<>();
-    ArrayList<String> c1 = new ArrayList<>();
+    ArrayList<String> recognizer1 = new ArrayList<>();
+    ArrayList<String> recognizer2 = new ArrayList<>();
+    ArrayList<String> recognizer3 = new ArrayList<>();
 
     public Recognizer(
             String word, FirstRecognizer firstRecognizer,
@@ -30,23 +30,24 @@ public class Recognizer {
 
         int index = 0;
         for (String[] array : dictionary) { //одно слово из юзера по всем категориям разными методами инжекс - ячейка
-            a1 = firstRecognizer.Search(array, word); //полное совпадение
-            b1 = secondRecognizer.Search(array, word);
-            c1 = thirdRecognizer.Search(array, word);
-            getBestWord(word, index, a1, b1, c1);
-            a1.clear();
-            b1.clear();
-            c1.clear();
+            recognizer1 = firstRecognizer.Search(array, word); //полное совпадение
+            recognizer2 = secondRecognizer.Search(array, word);
+            recognizer3 = thirdRecognizer.Search(array, word);
+            getBestWord(word, index, recognizer1, recognizer2, recognizer3);
+            recognizer1.clear();
+            recognizer2.clear();
+            recognizer3.clear();
             index++;
         }
     }
 
-    private void getBestWord(String word, int index, ArrayList<String> a1, ArrayList<String> b1, ArrayList<String> c1) {
-        if(a1.size() == 0) return;
+    private void getBestWord(String word, int index, ArrayList<String> recognizer1,
+                             ArrayList<String> recognizer2, ArrayList<String> recognizer3) {
+        if(recognizer1.size() == 0) return;
         ArrayList<String> all = new ArrayList<>();
-        all.add(String.valueOf(a1));
-        all.add(String.valueOf(b1));
-        all.add(String.valueOf(c1));
+        all.add(String.valueOf(recognizer1));
+        all.add(String.valueOf(recognizer2));
+        all.add(String.valueOf(recognizer3));
         StemmerGo stemmer = new StemmerGo(word);
         LevenshteinGo levenshtein = new LevenshteinGo(all, stemmer.stemmerWord);
         indexArray = index;
